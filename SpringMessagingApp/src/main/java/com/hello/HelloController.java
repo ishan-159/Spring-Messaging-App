d0d54c2f099c5815eb9f49d3d1bf5ffd5f7c4656
ishan-159 <ishantiwari159@gmail.com>
2025-02-21 16:26:27 +0530

    [Ishan] Add.Make rest call to show Hello Mark from BridgeLabz.

--- SpringMessagingApp/src/main/java/com/hello/HelloController.java
+++ SpringMessagingApp/src/main/java/com/hello/HelloController.java
@@ -1,6 +1,8 @@
 package com.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
@@ -8,6 +10,9 @@ public class HelloController {
                 public String hello() {
             return "Hello from BridgeLabz.";
         }
-
+        @GetMapping("/uc2")
+               public String SayHelloWithName(@RequestParam String name) {
+             return " Hello " + name + " from BridgeLabz";
+        }
     }
 
